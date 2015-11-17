92bdd36fafe34cc102f3cef90d75248b7bee8ce4
Zack Rosales <zrosales@cooksys.com>
2015-11-17 14:38:29 -0600

    Commiting to save progress so far. Not working yet.

--- src/com/cooksys/llc/main/LLCController.java
+++ src/com/cooksys/llc/main/LLCController.java
@@ -1,21 +1,28 @@
 package com.cooksys.llc.main;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
 @RestController
-@SpringBootApplication
 public class LLCController {
 
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+	
 	@RequestMapping(value="/zips/by/stateCounty/TX/Travis", method=RequestMethod.POST)
 	public @ResponseBody String zipsByStateCounty(@RequestBody String string){
 		return "test";
 	}
 	
+	@RequestMapping("/")
+	public Test test(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Test(counter.incrementAndGet(), String.format(template, name));
+	}
 }
