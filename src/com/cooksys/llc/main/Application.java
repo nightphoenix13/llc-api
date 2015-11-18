0b555d1006a7b75d3f31b1515ab7f9299be05982
Zack Rosales <zrosales@cooksys.com>
2015-11-18 15:52:23 -0600

    Added slf4j logger to Application and LLCController classes

--- src/com/cooksys/llc/main/Application.java
+++ src/com/cooksys/llc/main/Application.java
@@ -1,12 +1,21 @@
 package com.cooksys.llc.main;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
+		
+		logger.info("llc-api - About to launch application");
+		
 		SpringApplication.run(Application.class, args);
+		
+		logger.info("llp-api - Application launched");
 	}
 }
--- src/com/cooksys/llc/main/LLCController.java
+++ src/com/cooksys/llc/main/LLCController.java
@@ -2,11 +2,12 @@ package com.cooksys.llc.main;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,14 +15,21 @@ public class LLCController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	private final Logger logger = LoggerFactory.getLogger(LLCController.class);
 	
-	@RequestMapping(value="/zips/by/stateCounty/TX/Travis", method=RequestMethod.POST)
+	@RequestMapping(value="/zips/by/stateCounty/{state}/{county}", method=RequestMethod.POST)
 	public String zipsByStateCounty(@RequestBody String string){
+		
+		logger.info("llc-api - Rest call to /zips/by/stateCounty/{state}/{county} received");
+		
 		return "test";
 	}
 	
 	@RequestMapping("/test")
 	public Test test(@RequestParam(value="name", defaultValue="World") String name) {
+		
+		logger.info("llc-api - Rest call to /test received");
+		
 		return new Test(counter.incrementAndGet(), String.format(template, name));
 	}
 }
