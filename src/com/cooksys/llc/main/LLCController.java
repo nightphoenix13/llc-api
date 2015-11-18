71b69f920384b4bef13ce1f11c4deef3d12c8ccb
Zack Rosales <zrosales@cooksys.com>
2015-11-18 09:12:12 -0600

    Committing latest work so it can be reviewed to see if someone can figure out why the rest calls aren't working.

--- src/com/cooksys/llc/main/LLCController.java
+++ src/com/cooksys/llc/main/LLCController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class LLCController {
 
 	private static final String template = "Hello, %s!";
